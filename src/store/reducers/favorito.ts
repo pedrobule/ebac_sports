5c615597ae0f7e7201d1e115c1139eb31fedeaed
Pedro Cavalcante <pedrowinie@gmail.com>
2025-03-19 09:06:19 -0300

    commit final

---
+++ src/store/reducers/favorito.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type FavoritosState = {
+  itens: Produto[]
+}
+
+const initialState: FavoritosState = {
+  itens: []
+}
+
+const favoritosSlice = createSlice({
+  name: 'favoritos',
+  initialState,
+  reducers: {
+    adicionar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        alert('Produto já está nos favoritos!')
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { adicionar } = favoritosSlice.actions
+export default favoritosSlice.reducer
